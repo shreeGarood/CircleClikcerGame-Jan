d3011fcb52c33626877b86ed4496beee007efd17
shreeGarood <shrinath.himane@mindbowser..com>
2025-01-23 16:37:56 +0530

    Deploy production

--- src/app/api/scores/[id]/route.ts
+++ src/app/api/scores/[id]/route.ts
@@ -1,14 +1,14 @@
 import { db } from "@/utils/firebaseConfig";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
-import type { NextApiRequest, NextApiResponse } from "next";
 
 // PATCH Route
 export async function PATCH(request: Request, context: { params: Record<string, string> }) {
   try {
-    const { id } = context.params; // Access the dynamic parameter
-    const { score } = await request.json(); // Get data from the request body
+    const { id } = context.params;
+    const { score } = await request.json();
     const scoreDoc = doc(db, "scores", id);
+
     await updateDoc(scoreDoc, { score });
     return NextResponse.json({ message: "Score updated successfully", id, score });
   } catch (error) {
@@ -22,8 +22,9 @@ export async function PATCH(request: Request, context: { params: Record<string, 
 // DELETE Route
 export async function DELETE(request: Request, context: { params: Record<string, string> }) {
   try {
-    const { id } = context.params; // Access the dynamic parameter
+    const { id } = context.params;
     const scoreDoc = doc(db, "scores", id);
+
     await deleteDoc(scoreDoc);
     return NextResponse.json({ message: "Score deleted successfully", id });
   } catch (error) {
